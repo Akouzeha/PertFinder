b96fca3196703e8b21ed8f905c347591e1a6b4c9
Akouzeha <ammar.kouzeha@etu.unistra.fr>
2023-11-01 11:51:24 +0100

    registeration form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,27 +3,52 @@
 namespace App\Form;
 
 use App\Entity\User;
+use PharIo\Manifest\Email;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use PHPUnit\TextUI\XmlConfiguration\CodeCoverage\Report\Text;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
-            ->add('nom')
-            ->add('prenom')
-            ->add('email')
+            ->add('username', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'Nom d\'utilisateur'
+                ]
+            ])
+            ->add('nom', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'Nom'
+                ]
+            ])
+            ->add('prenom', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'Prénom'
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'attr' => [
+                    'placeholder' => 'Adresse email'
+                ]
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
+                'label' => 'J\'accepte les',
+                
+                'attr' => [
+                    'class' => 'form-check-input'
+                ],
                 'constraints' => [
                     new IsTrue([
                         'message' => 'You should agree to our terms.',
@@ -38,8 +63,12 @@ class RegistrationFormType extends AbstractType
                 'invalid_message' => 'The password fields must match.',
                 'options' => ['attr' => ['class' => 'password-field']],
                 'required' => true,
-                'first_options'  => ['label' => 'Password'],
-                'second_options' => ['label' => 'Repeat Password'],
+                'first_options'  => ['label' => 'Password','attr' => [
+                    'placeholder' => 'Mot de passe'
+                ]],
+                'second_options' => ['label' => 'Repeat Password','attr' => [
+                    'placeholder' => 'Répéter le mot de passe'
+                ]],
                 
             ])
         ;
