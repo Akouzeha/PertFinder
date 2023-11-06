c8243e50c1223ca7189e7dac880515fc7313ffec
Akouzeha <ammar.kouzeha@etu.unistra.fr>
2023-11-06 13:51:30 +0100

    navBar

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,8 @@
-// Get the current page's URL from Symfony's path function
-var currentPageURL = "{{ app.request.attributes.get('_route') }}";
-
-// Find the corresponding link and add the 'active' class
-var navLinks = document.querySelectorAll('.navItem a');
-for (var i = 0; i < navLinks.length; i++) {
-  if (navLinks[i].getAttribute('href') === currentPageURL) {
-    navLinks[i].parentElement.classList.add('activated');
+function myFunction() {
+  var x = document.getElementById("myNavBar");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
 }
