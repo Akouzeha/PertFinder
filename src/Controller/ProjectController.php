3fbd161258a6af834992ba2e6a4db3f8d53a6863
Akouzeha <ammar.kouzeha@etu.unistra.fr>
2023-12-19 16:11:34 +0100

    add a new role - edit comment

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -65,22 +65,8 @@ class ProjectController extends AbstractController
         ]);
     }
 
-    /**
-     * @Route("/project/{id}", name="delete_project")
-     * @return Response
-     * delete a project
-     */
-
-    #[Route('/project/{id}', name: 'delete_project')]
-    public function deleteProject(Project $project, EntityManagerInterface $entityManager): Response
-    {
-        $entityManager->remove($project);
-        $entityManager->flush();
-        return $this->redirectToRoute('app_project');
-    }
-
     #[Route('/project/show/{id}', name: 'show_project')]
-    public function showProject(Project $project, EntityManagerInterface $em,Request $request): Response
+    public function showProject(Project $project, EntityManagerInterface $em,Request $request, Comment $comment = NULL): Response
     {
         if ($project->getDiagrams() == null) {
             $imgName = 'default.png';
@@ -88,6 +74,7 @@ class ProjectController extends AbstractController
             $diagrams = $project->getDiagrams();
             if ($diagrams[0] != null) {
                 $diagramName = $diagrams[0]->getTitle();
+                $diagramId = $diagrams[0]->getId();
                 $imgName = str_replace(' ', '_', $diagramName);
             } else {
                 $imgName = 'default.png';
@@ -96,14 +83,20 @@ class ProjectController extends AbstractController
         $projectId = $project->getId();
         //add a comment to the project
         $user = $this->getUser();
-        $comment = new Comment();
+        if(!$comment){
+            $comment = new Comment();
+        }
+
+        $commentId = $comment->getId();
         $comment->setProject($project);
         $comment->setUser($user);
         $comment->setCommentTime(new \DateTime());
         $form = $this->createForm(CommentType::class, $comment);
+
         $form->handleRequest($request);
         // Check if the form is submitted and valid
         if ($form->isSubmitted() && $form->isValid()) {
+            
             $comment = $form->getData();
             $em->persist($comment);
             $em->flush();
@@ -112,9 +105,64 @@ class ProjectController extends AbstractController
         
         return $this->render('project/show.html.twig',[
             'project' => $project,
+            'commentId' => $commentId,
+            'diagramId' => $diagramId,
             'imgName' => $imgName . '.png',
             'formComment' => $form,
         ]);
     }
+
+    /**
+     * @Route("/project/show/{commentId}", name="edit_comment")
+     * @return Response
+     * edit a comment
+     */
+    #[Route('/project/show/{id}/{commentId}', name: 'edit_comment')]
+    public function editComment($commentId, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $comment = $entityManager->getRepository(Comment::class)->find($commentId);
+        $projectId = $comment->getProject()->getId();
+        $project = $entityManager->getRepository(Project::class)->find($projectId);
+        if ($project->getDiagrams() == null) {
+            $imgName = 'default.png';
+        } else {
+            $diagrams = $project->getDiagrams();
+            if ($diagrams[0] != null) {
+                $diagramName = $diagrams[0]->getTitle();
+                $diagramId = $diagrams[0]->getId();
+                $imgName = str_replace(' ', '_', $diagramName);
+            } else {
+                $imgName = 'default.png';
+            }
+        }
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+        // Check if the form is submitted and valid
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment = $form->getData();
+            $entityManager->persist($comment);
+            $entityManager->flush();
+            return $this->redirectToRoute('show_project', ['id' => $projectId]);
+        }
+        return $this->render('project/show.html.twig', [
+            'formEditComment' => $form,
+            'project' => $project,
+            'imgName' => $imgName . '.png',
+        ]);
+    }
+
+    /**
+     * @Route("/project/{id}", name="delete_project")
+     * @return Response
+     * delete a project
+     */
+
+     #[Route('/project/{id}', name: 'delete_project')]
+     public function deleteProject(Project $project, EntityManagerInterface $entityManager): Response
+     {
+         $entityManager->remove($project);
+         $entityManager->flush();
+         return $this->redirectToRoute('app_project');
+     }
 
 }
