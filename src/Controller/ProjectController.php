2e6258e156aaed32eec553a8cf4389e1634e76c5
Akouzeha <ammar.kouzeha@etu.unistra.fr>
2024-01-19 16:52:58 +0100

    add permession to edit project

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -45,7 +45,7 @@ class ProjectController extends AbstractController
     public function createProject(Project $project = Null, Request $request, EntityManagerInterface $entityManager): Response
     {
         $user = $this->getUser();
-        if($project->getId() != null && $project->getUser() != $user){
+        if($project->getId() != null && ($project->getUser() != $user && !$this->isGranted('ROLE_PROJECT_MANAGER'))){
             $this->addFlash('error', 'Vous n\'avez pas le droit de modifier ce projet');
             return $this->redirectToRoute('app_project');
         }
